1942778c652ee33dd46a1fde5c8cbc5aa727c389
Arturo Martinez <d.martinez@sisproconsultores.com>
2020-02-19 10:17:41 -0500

    Adding header and links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 const HatsPage = () => (
   <div>
@@ -15,6 +16,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/hats" component={HatsPage} />
